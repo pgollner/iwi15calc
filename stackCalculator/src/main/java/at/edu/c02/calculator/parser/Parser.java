e7a7f9df13785c4edf347aadde5591e178ea04cb
patrickposch <patrick.posch@edu.campus02.at>
2018-01-13 10:42:54 +0100

    all done

--- stackCalculator/src/main/java/at/edu/c02/calculator/parser/Parser.java
+++ stackCalculator/src/main/java/at/edu/c02/calculator/parser/Parser.java
@@ -78,6 +78,9 @@ public class Parser {
 			return Operation.div;
 		else if ("-".equals(value))
 			return Operation.sub;
+		else if ("%".equals(value))
+			return Operation.div;
+			
 		
 		throw new CalculatorException("Unsuppoted Operation");
 	}
